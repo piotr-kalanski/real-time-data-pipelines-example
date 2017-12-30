47289f79be7f44a4cb6859740abe5a0c53d17dd8
piotr-kalanski <piotr.kalanski@gmail.com>
2017-12-30 19:40:20 +0100

    Clickstream import connector configuration

--- clickstream-import/create_converter.sh
+++ clickstream-import/create_converter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s -X POST -H "Content-Type: application/json" --data "@clickstream_import.json"
+curl -s -X POST -H "Content-Type: application/json" --data '@clickstream-import.json' http://127.0.0.1:8083/connectors
