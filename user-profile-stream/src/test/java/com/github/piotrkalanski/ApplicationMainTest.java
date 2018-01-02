74c76d7b8843456a4acd76e4b2af23a0c1757bd6
piotr-kalanski <piotr.kalanski@gmail.com>
2018-01-02 17:54:38 +0100

    Example unit tests for user profile

---
+++ user-profile-stream/src/test/java/com/github/piotrkalanski/ApplicationMainTest.java
@@ -0,0 +1,102 @@
+package com.github.piotrkalanski;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ApplicationMainTest {
+
+    private ApplicationMain app = new ApplicationMain();
+
+    @Test
+    public void testCalculateFavouriteDevice() {
+        List<DeviceUsage> devices = Arrays.asList(
+                DeviceUsage.newBuilder().setDevice("DESKTOP").setActionsCount(2L).build(),
+                DeviceUsage.newBuilder().setDevice("MOBILE").setActionsCount(1L).build()
+        );
+
+        assertEquals("DESKTOP", app.calculateFavouriteDevice(devices));
+    }
+
+    @Test
+    public void testCalculateDeviceUsageStartingWithEmptyList() {
+        List<DeviceUsage> oldDevices = new LinkedList<>();
+        List<DeviceUsage> expectedDevices = Arrays.asList(
+                DeviceUsage.newBuilder().setDevice("DESKTOP").setActionsCount(1L).build()
+        );
+        clickstream action = clickstream
+                .newBuilder()
+                .setEventId(1)
+                .setEventType("LV")
+                .setEventDate(DateTime.now())
+                .setDevice("DESKTOP")
+                .build();
+
+        assertEquals(expectedDevices, app.calculateDeviceUsage(action, oldDevices));
+    }
+
+    @Test
+    public void testCalculateDeviceUsageWithEventWithoutDevice() {
+        List<DeviceUsage> oldDevices = Arrays.asList(
+                DeviceUsage.newBuilder().setDevice("DESKTOP").setActionsCount(1L).build()
+        );
+        List<DeviceUsage> expectedDevices = Arrays.asList(
+                DeviceUsage.newBuilder().setDevice("DESKTOP").setActionsCount(1L).build()
+        );
+        clickstream action = clickstream
+                .newBuilder()
+                .setEventId(1)
+                .setEventType("LV")
+                .setEventDate(DateTime.now())
+                .build();
+
+        assertEquals(expectedDevices, app.calculateDeviceUsage(action, oldDevices));
+    }
+
+    @Test
+    public void testCalculateDeviceUsageWithNewDevice() {
+        List<DeviceUsage> oldDevices = new LinkedList<>();
+        oldDevices.add(
+                DeviceUsage.newBuilder().setDevice("DESKTOP").setActionsCount(1L).build()
+        );
+        List<DeviceUsage> expectedDevices = Arrays.asList(
+                DeviceUsage.newBuilder().setDevice("DESKTOP").setActionsCount(1L).build(),
+                DeviceUsage.newBuilder().setDevice("MOBILE").setActionsCount(1L).build()
+        );
+        clickstream action = clickstream
+                .newBuilder()
+                .setEventId(1)
+                .setEventType("LV")
+                .setEventDate(DateTime.now())
+                .setDevice("MOBILE")
+                .build();
+
+        assertEquals(expectedDevices, app.calculateDeviceUsage(action, oldDevices));
+    }
+
+    @Test
+    public void testCalculateDeviceUsageWithExistingDevice() {
+        List<DeviceUsage> oldDevices = Arrays.asList(
+                DeviceUsage.newBuilder().setDevice("DESKTOP").setActionsCount(1L).build(),
+                DeviceUsage.newBuilder().setDevice("MOBILE").setActionsCount(1L).build()
+        );
+        List<DeviceUsage> expectedDevices = Arrays.asList(
+                DeviceUsage.newBuilder().setDevice("DESKTOP").setActionsCount(2L).build(),
+                DeviceUsage.newBuilder().setDevice("MOBILE").setActionsCount(1L).build()
+        );
+        clickstream action = clickstream
+                .newBuilder()
+                .setEventId(1)
+                .setEventType("LV")
+                .setEventDate(DateTime.now())
+                .setDevice("DESKTOP")
+                .build();
+
+        assertEquals(expectedDevices, app.calculateDeviceUsage(action, oldDevices));
+    }
+}
